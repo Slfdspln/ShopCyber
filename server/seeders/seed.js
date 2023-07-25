cbd52488b62b507647aad7bea6d136a1bbe0ec80
Radhika Bandi <radhika.shetti@gendigital.com>
2023-07-25 15:07:32 -0700

    adding product details page and fix query

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -22,7 +22,7 @@ db.once('open', async () => {
         name: 'Cookies',
         price: 2.99,
         description:
-          'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+          'Delicious tin of cookies.',
         image: 'cookie-tin.jpg',
         quantity: 500,
         category: categories[0]._id,
@@ -33,8 +33,8 @@ db.once('open', async () => {
         name: 'Handmade Soap',
         category: categories[1]._id,
         description:
-          'Praesent placerat, odio vel euismod venenatis, lectus arcu laoreet felis, et fringilla sapien turpis vestibulum nisl.',
-        image: 'soap.jpg',
+          'Handmade Soap-Handmade Soap',
+           image: 'soap.jpg',
         price: 3.99,
         quantity: 50
       },
@@ -43,7 +43,7 @@ db.once('open', async () => {
         name: 'Camera',
         category: categories[2]._id,
         description:
-          'Vestibulum risus metus, luctus non tortor quis, tincidunt consectetur ex. Nullam vitae lobortis ligula, ut sagittis massa. Curabitur consectetur, tellus at pulvinar venenatis, erat augue cursus erat, eu ullamcorper eros lectus ultrices ipsum. Integer rutrum, augue vitae auctor venenatis, turpis turpis elementum orci, at sagittis risus mi a leo.',
+          'evice for recording an image of an object on a light-sensitive surface',
         image: 'camera.jpg',
         price: 399.99,
         quantity: 30
@@ -53,8 +53,8 @@ db.once('open', async () => {
         name: 'Tales at Bedtime',
         category: categories[3]._id,
         description:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare diam quis eleifend rutrum. Aliquam nulla est, volutpat non enim nec, pharetra gravida augue. Donec vitae dictum neque. Pellentesque arcu lorem, fringilla non ligula ac, tristique bibendum erat. Ut a semper nibh. Quisque a mi et mi tempor ultricies. Maecenas eu ipsum eu enim hendrerit accumsan at euismod urna.',
-        image: 'bedtime-book.jpg',
+          'bedtime story book',
+           image: 'bedtime-book.jpg',
         price: 9.99,
         quantity: 100
       },
